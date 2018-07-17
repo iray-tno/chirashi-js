65dfd2931d209bb5b85208e8445215945eb497ea
iray-tno <iray.tno@gmail.com>
2018-07-18 02:16:34 +0900

    Add my precious first page

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Root extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
