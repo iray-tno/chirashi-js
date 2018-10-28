1c1b99bc8b2324da422702d8a76fdd1a44df1288
iray-tno <iray.tno@gmail.com>
2018-10-29 01:33:52 +0900

    Set up test environment with jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+    verbose: true,
+    testPathIgnorePatterns: [
+        ...defaults.testPathIgnorePatterns,
+        '/.cache/',
+    ],
+};
