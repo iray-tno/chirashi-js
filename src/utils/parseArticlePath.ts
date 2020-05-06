3ef04557e6970aba8504591120c440a9addb5027
iray-tno <iray.tno@gmail.com>
2020-05-07 00:21:50 +0900

    #81 Migrate utils to TypeScript

--- src/utils/parseArticlePath.ts
+++ src/utils/parseArticlePath.ts
@@ -1,8 +1,6 @@
 const pattern = /^\/(\d{4}-\d{1,2}-\d{1,2})_(\d{2})_(.+)\/$/;
 
-function parseArticlePath(path) {
+export default function parseArticlePath(path: string) {
     const [/* ignore */, date, index, name] = path.match(pattern);
     return [date, index, name];
 }
-
-module.exports = parseArticlePath;
