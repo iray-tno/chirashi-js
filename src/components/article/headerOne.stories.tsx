b2f33d1077b1ea7dcd8f28ca8123ffaefc915399
iray-tno <iray.tno@gmail.com>
2020-06-03 04:01:54 +0900

    #162 Add h1 components

---
+++ src/components/article/headerOne.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import HeaderOne from './HeaderOne';
+
+export default {
+    title: 'article/HeaderOne',
+};
+
+export const defaultView: React.ReactNode = () => {
+    return <HeaderOne>Title Sample</HeaderOne>;
+};
