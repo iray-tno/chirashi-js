7856a787e698f121c3d55c5bb136061bcf00551e
iray-tno <iray.tno@gmail.com>
2020-08-20 03:37:17 +0900

    #201 Add indentation style

--- src/components/tableOfContents/TableOfContents.tsx
+++ src/components/tableOfContents/TableOfContents.tsx
@@ -20,18 +20,33 @@ const TableOfContents: React.FC<Props> = React.memo((props) => {
         tableOfContents,
     } = props;
 
-    const lastItemId = tableOfContents == null ? null : tableOfContents.lastItemId;
     if (headings == null) return null;
+
+    const lastItemId = tableOfContents == null ? null : tableOfContents.lastItemId;
+    const minDepth = headings.reduce((currentValue, item) => {
+        if (item?.depth == null) return currentValue;
+        return currentValue == null ? item.depth : Math.min(item.depth, currentValue);
+    }, 6);
+
     return (
         <div className={className} styleName="outer">
             {headings.map((headerItem) => {
                 if (headerItem?.id == null) return null;
 
                 const isInView = tableOfContents?.items[headerItem.id]?.isInView;
+                const itemRelariveDepth = (headerItem.depth || minDepth) - minDepth + 1;
+
                 const itemClassNames = classNames({
                     item: true,
                     active: isInView || lastItemId === headerItem.id,
+                    depthOne: itemRelariveDepth === 1,
+                    depthTwo: itemRelariveDepth === 2,
+                    depthThree: itemRelariveDepth === 3,
+                    depthFour: itemRelariveDepth === 4,
+                    depthFive: itemRelariveDepth === 5,
+                    depthSix: itemRelariveDepth === 6,
                 });
+
                 return (
                     <li key={headerItem.id} styleName={itemClassNames}>
                         <a styleName="link" href={`#${headerItem.id}`}>
