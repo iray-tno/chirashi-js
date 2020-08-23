62b7871dbd9282482fa2af8e30d461208776d5a6
iray-tno <iray.tno@gmail.com>
2020-08-24 02:47:56 +0900

    #323 Add parsing of tag attributes in frontmatter

--- src/components/frontmatter/TagItem.tsx
+++ src/components/frontmatter/TagItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import './tagItem.module.scss';
 
@@ -8,14 +9,42 @@ type Props = {
     styleName?: string,
 };
 
-const TagItem: React.FC<Props> = React.memo((props) => {
-    const originalTag = props.tag;
+type Tag = {
+    tagName: string,
+    tagId: string,
+};
 
-    const tagName = originalTag;
+function parseTag(tag: string) : Tag {
+    const startIndex = tag.indexOf('(');
+    const endIndex = tag.indexOf(')');
+    if (startIndex < 0 && endIndex < 0) {
+        return {
+            tagName: tag,
+            tagId: tag,
+        };
+    }
+
+    return {
+        tagName: tag.substring(0, startIndex),
+        tagId: tag.substring(startIndex + 1, endIndex),
+    };
+}
+
+const TagItem: React.FC<Props> = React.memo((props) => {
+    const {
+        tag: originalTag,
+        className,
+    } = props;
+
+    const {
+        tagName,
+        tagId,
+    } = parseTag(originalTag);
+
     return (
-        <span className={props.className} styleName="tagItem">
+        <span className={className} styleName="tagItem">
             <span styleName="name">
-                {tagName}
+                <Link to={`/tags/${tagId}`}>{tagName}</Link>
             </span>
         </span>
     );
